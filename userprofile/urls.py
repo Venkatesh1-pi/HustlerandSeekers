19d96a5970d924edde206eb508a8a51f71bd1898
pydev1996 <120152785+pydev1996@users.noreply.github.com>
2025-04-11 16:55:25 +0530

    Delete userprofile directory

--- userprofile/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ViewProfile, UpdateProfile
-
-urlpatterns = [
-    path('view/', ViewProfile.as_view(), name='view-profile'),
-    path('update/', UpdateProfile.as_view(), name='update-profile'),
-]
